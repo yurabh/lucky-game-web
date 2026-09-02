9de5101dda684eb5d4d6592d9b5abbfd369b8b3f
Yuriy <yurabh1990@gmail.com>
2026-09-02 13:38:01 +0300

    Fix migration games

--- database/migrations/2026_09_01_000000_create_games_table.php
+++ database/migrations/2026_09_01_000000_create_games_table.php
@@ -4,14 +4,13 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
     public function up(): void
     {
-        Schema::create('game_results', function (Blueprint $table) {
+        Schema::create('games', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id')->constrained()->cascadeOnDelete();
             $table->unsignedSmallInteger('random_number');
@@ -28,6 +27,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('game_results');
+        Schema::dropIfExists('games');
     }
 };
